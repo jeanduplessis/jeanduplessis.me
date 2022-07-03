cb5aaa7ee2f78e402053a32af7acee58b2207e6c
Jean du Plessis <jeandp@gmail.com>
2022-07-03 21:33:31 +0200

    Upgrade packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
       fontFamily: {
        'sans': ['Roboto','Helvetica', 'Arial', 'sans-serif'],
@@ -10,10 +8,6 @@ module.exports = {
       },
     letterSpacing: { widest: "0.25em" },
     extend: {},
-  },
-
-  variants: {
-    extend: {},
-  },
+  },  
   plugins: [require("@tailwindcss/typography")],
 };
