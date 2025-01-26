fc751e5647f0c19f6261b600fa495f20284577f2
Jean du Plessis <jean@upbound.io>
2025-01-26 15:56:49 +0200

    Add Vercel analytics and speed insights

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -44,6 +45,8 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <SpeedInsights />
+          <Analytics />
         </body>
       </Html>
     );
