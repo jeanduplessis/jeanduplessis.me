6b391a37a2373bd311976057e7a1aa01cb177f21
Jean du Plessis <jean@upbound.io>
2025-01-26 14:00:23 +0200

    npx @next/codemod@latest next-image-to-legacy-image .

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 export default function Footer() {
   return (
